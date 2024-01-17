8fda41272f6107331cd0c60c7de293f5ed523e6e
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-01-17 09:20:30 -0500

    Update install-java.sh

--- startupscript/install-java.sh
+++ startupscript/install-java.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-emit "Installing Java JDK ..."
 
-# Create a soft link in /usr/bin to the java runtime
+# install-java.sh
+#
+# Creates a soft link in /usr/bin to the java runtime.
+#
+# Note that this script is intended to be source from the "post-startup.sh" script
+# and is dependent on java packages already installed in the container image or as 
+# a devcontainer feature (ghcr.io/devcontainers/features/java:1).
+
 ln -sf "$(which java)" "/usr/bin"
 chown --no-dereference "${user}" "/usr/bin/java"
 

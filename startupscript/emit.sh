f475229b3add3fc9bf444bc59f7f742f7afff9e7
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-01-17 13:24:37 -0500

    Update emit.sh

--- startupscript/emit.sh
+++ startupscript/emit.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# emit.sh
+#
+# Creates a bash function that can be used for emitting logging messages with a date/timestamp
+
 function emit() {
  echo "$(date '+%Y-%m-%d %H:%M:%S') $*"
 }

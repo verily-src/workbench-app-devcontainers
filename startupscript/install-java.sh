0359b7ba2bf29d15be66f76c83e42f709170c031
Yu Hu <yuhuyoyo@google.com>
2024-01-23 10:30:51 -0500

    rename variable

--- startupscript/install-java.sh
+++ startupscript/install-java.sh
@@ -4,10 +4,13 @@
 #
 # Creates a soft link in /usr/bin to the java runtime.
 #
-# Note that this script is intended to be source from the "post-startup.sh" script
-# and is dependent on java packages already installed in the container image or as 
+# Note that this script is intended to be sourced from the "post-startup.sh" script
+# and is dependent on some variables and packages already being set up:
+# 
+# - java packages already installed in the container image or as 
 # a devcontainer feature (ghcr.io/devcontainers/features/java:1).
+# - USER_NAME: app user
 
 ln -sf "$(which java)" "/usr/bin"
-chown --no-dereference "${user}" "/usr/bin/java"
+chown --no-dereference "${USER_NAME}" "/usr/bin/java"
 

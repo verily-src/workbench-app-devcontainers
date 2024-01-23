46f07880f242f696ac32c93d8919017ff3a0760b
Yu Hu <yuhuyoyo@google.com>
2024-01-23 14:18:42 -0500

    add more comment

--- startupscript/install-java.sh
+++ startupscript/install-java.sh
@@ -9,13 +9,9 @@
 # 
 # - java packages already installed in the container image or as 
 # a devcontainer feature (ghcr.io/devcontainers/features/java:1).
+# - USER_NAME: app user name
 
 # Check if the user name is provided.
-if [[ -z "$1" ]]; then
-    echo "usage: install-java.sh <user>."
-    exit 1
-fi
-readonly user="${1}"
 ln -sf "$(which java)" "/usr/bin"
-chown --no-dereference "${user}" "/usr/bin/java"
+chown --no-dereference "${USER_NAME}" "/usr/bin/java"
 

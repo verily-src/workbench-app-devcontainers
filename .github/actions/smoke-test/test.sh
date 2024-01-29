712c99becad0eeae606e0e483740e4bcfc166acc
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-01-29 15:57:45 -0500

    Update test.sh

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -15,20 +15,20 @@ echo "Running Smoke Test"
 readonly ID_LABEL="test-container=${TEMPLATE_ID}"
 devcontainer exec \
   --workspace-folder "${SRC_DIR}" \
-  --id-label ${ID_LABEL}
-/bin/sh -c '\
-  set -o errexit && \
-  if [[ -f "test-project/test.sh" ]]; then \
-    cd test-project && \
-    if [[ "$(id -u)" == "0" ]]; then \
-      chmod +x test.sh \
+  --id-label "${ID_LABEL}" \
+  /bin/sh -c '\
+    set -o errexit && \
+    if [[ -f "test-project/test.sh" ]]; then \
+      cd test-project && \
+      if [[ "$(id -u)" == "0" ]]; then \
+        chmod +x test.sh \
+      else \
+        sudo chmod +x test.sh; \
+      fi && \
+      ./test.sh; \
     else \
-      sudo chmod +x test.sh; \
-    fi && \
-    ./test.sh; \
-  else \
-    ls -a; \
-  fi'
+      ls -a; \
+    fi'
 
 # Clean up
 docker rm -f "$(docker container ls -f \"label=${ID_LABEL}\" -q)"

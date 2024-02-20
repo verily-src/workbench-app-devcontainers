a54c0f082271d74694942a2d11e30aa3ea33b3b1
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-02-20 08:44:57 -0500

    add comment

--- startupscript/gcp/resource-mount.sh
+++ startupscript/gcp/resource-mount.sh
@@ -28,7 +28,8 @@ else
   emit "gcsfuse already installed. Skipping installation."
 fi
 
-#sed -i '/user_allow_other/s/^#//g' /etc/fuse.conf
+# Uncomment user_allow_other in the fuse.conf to enable non-root user to mount files with -o allow-other option.
+sed -i '/user_allow_other/s/^#//g' /etc/fuse.conf
 
 if [[ "${LOG_IN}" == "true" ]]; then
   ${RUN_AS_LOGIN_USER} "wb resource mount --allow-other"

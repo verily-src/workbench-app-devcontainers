d71a56da089661a5ce20805e07d50c7570ba72dc
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-03-05 11:15:04 -0500

    add comment about removing this

--- startupscript/vertex-ai-user-managed-notebook/post-startup.sh
+++ startupscript/vertex-ai-user-managed-notebook/post-startup.sh
@@ -239,7 +239,8 @@ readonly NOTEBOOK_CONFIG
 emit "Resynchronizing apt package index..."
 
 # The apt package index may not be clean when we run; resynchronize
-# Update apt to point to the new k8s pkg. https://kubernetes.io/blog/2023/08/15/pkgs-k8s-io-introduction/
+# TODO (BENCH-2316): Update apt to point to the new k8s pkg. https://kubernetes.io/blog/2023/08/15/pkgs-k8s-io-introduction/
+# Remove this when deep learning image fix this issue.
 mkdir /etc/apt/keyrings
 chmod 755 /etc/apt/keyrings
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list

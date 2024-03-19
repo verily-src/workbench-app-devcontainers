9ff64e1959d381a7d5160223a15888f3a483d186
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-03-19 17:25:07 -0400

    Update src/cirrocumulus/scripts/cirro_scanner.sh
    Co-authored-by: Matt Bookman <mbookman@google.com>

--- src/cirrocumulus/scripts/cirro_scanner.sh
+++ src/cirrocumulus/scripts/cirro_scanner.sh
@@ -40,6 +40,7 @@ function scan_folders_and_create_datasets() {
 readonly -f scan_folders_and_create_datasets
 
 wb resource mount
+
 # Infinite loop to continuously scan every 5 seconds
 while true; do
     # Call the function to perform the scanning

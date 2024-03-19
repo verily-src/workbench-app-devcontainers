0e56852bcfabdc11cccc814c6b9438070118b85b
John Czerkowicz <jczerk@verily.com>
2024-03-19 13:45:20 -0400

    fixup

--- startupscript/aws/post-startup-hook.sh
+++ startupscript/aws/post-startup-hook.sh
@@ -19,10 +19,8 @@
 # - USER_PRIMARY_GROUP: name of primary group app user belongs to
 # - USER_WORKBENCH_CONFIG_DIR: user's WB configuration directory
 # - WORK_DIRECTORY: home directory for the user that the script is running on behalf of
-# - WORKBENCH_GIT_REPOS_DIR: path to the git repo directory (~/repos)
 # - WORKBENCH_INSTALL_PATH: path to CLI executable
 
-readonly USER_PROFILE="${WORK_DIRECTORY}/.profile"
 readonly AWS_VAULT_INSTALL_PATH="/usr/bin/aws-vault"
 readonly AWS_VAULT_EXE_URL="https://github.com/99designs/aws-vault/releases/download/v7.2.0/aws-vault-linux-amd64"
 
@@ -40,22 +38,24 @@ ${RUN_AS_LOGIN_USER} "${WORKBENCH_INSTALL_PATH} config set cache-with-aws-vault 
 ${RUN_AS_LOGIN_USER} "${WORKBENCH_INSTALL_PATH} config set wb-path --path ${WORKBENCH_INSTALL_PATH}"
 ${RUN_AS_LOGIN_USER} "${WORKBENCH_INSTALL_PATH} config set aws-vault-path --path ${AWS_VAULT_INSTALL_PATH}"
 
-#######################################################################################
-# Write common environment vars to /etc/environment so they are present in all contexts
-#######################################################################################
+#################################################
+# Write common environment vars to user's .bashrc
+#################################################
 WORKBENCH_WORKSPACE="$(get_metadata_value terra-workspace-id)"
 readonly WORKBENCH_WORKSPACE
 AWS_CONFIG_FILE="${USER_WORKBENCH_CONFIG_DIR}/aws/${WORKBENCH_WORKSPACE}.conf"
 readonly AWS_CONFIG_FILE
-cat >> /etc/environment << EOF
-WORKBENCH_WORKSPACE="${WORKBENCH_WORKSPACE}"
-WORKBENCH_GIT_REPOS_DIR="${WORKBENCH_GIT_REPOS_DIR}"
-WORKBENCH_INSTALL_PATH="${WORKBENCH_INSTALL_PATH}"
-AWS_CONFIG_FILE="${AWS_CONFIG_FILE}"
-AWS_VAULT_BACKEND="file"
-AWS_VAULT_FILE_PASSPHRASE=""
+
+cat > "${USER_BASHRC}" << EOF
+
+# AWS-specific Workbench Configuration Environment Variables
+export WORKBENCH_WORKSPACE="${WORKBENCH_WORKSPACE}"
+export WORKBENCH_GIT_REPOS_DIR="${WORK_DIRECTORY}/repos"
+export WORKBENCH_INSTALL_PATH="${WORKBENCH_INSTALL_PATH}"
+export AWS_CONFIG_FILE="${AWS_CONFIG_FILE}"
+export AWS_VAULT_BACKEND="file"
+export AWS_VAULT_FILE_PASSPHRASE=""
 EOF
-chown "${USER_NAME}:${USER_PRIMARY_GROUP}" "${USER_PROFILE}"
 
 #######################################
 # Write VWB helper functions to .bashrc

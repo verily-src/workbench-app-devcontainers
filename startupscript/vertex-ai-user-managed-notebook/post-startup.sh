d781f194e969736dac94ff95d4616e290911fc75
Yu Hu <yuhuyoyo@google.com>
2024-03-19 16:41:05 -0400

    bracket

--- startupscript/vertex-ai-user-managed-notebook/post-startup.sh
+++ startupscript/vertex-ai-user-managed-notebook/post-startup.sh
@@ -204,11 +204,11 @@ function retry () {
 
   # If the exit code is non-zero (i.e. command failed), and we have not
   # reached the maximum number of retries, run the command again
-  if [[ $exit_code -ne 0 && $retries -gt 0 ]]; then
-    retry $(($retries - 1)) "$command"
+  if [[ ${exit_code} -ne 0 && ${retries} -gt 0 ]]; then
+    retry $((retries - 1)) "${command}"
   else
     # Return the exit code from the command
-    return $exit_code
+    return ${exit_code}
   fi
 }
 readonly -f retry

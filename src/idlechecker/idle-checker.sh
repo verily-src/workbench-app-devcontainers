612784c8e1931f7bfc94ae21dc6279ea9085c57b
Yu Hu <yuhuyoyo@google.com>
2024-03-25 16:14:41 -0400

    address comments

--- src/idlechecker/idle-checker.sh
+++ src/idlechecker/idle-checker.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-# idle-checker.sh checks for cpu utilization of the VM and update last active timestamp if the CPU utilization
+# idle-checker.sh checks for CPU utilization of the VM and updates last active timestamp if the CPU utilization
 # is above a given threshold.
 #
-# Note that this script is dependent on some variables and packages being set up:
+# Note that this script is dependent on some environment variables and packages being set up:
 #
-# - AWS CLI is installed
+# Environment variables:
+# - CLOUD: cloud platform VM is running on
+#
+# Software:
+# - AWS CLI
+
 # - CLOUD: cloud platform VM is running on
 # - bc https://manpages.ubuntu.com/manpages/trusty/en/man1/bc.1.html
 
 set -o errexit
-set -o nounset
+
 set -o pipefail
 set -o xtrace
 
 # threshold - threshold for cpu usage, used to determine if instance is idle. If usage goes above this number count resets to zero. By default 0.1 (10 percent)
-readonly threshold="${THRESHOLD:-0.1}"
+readonly THRESHOLD="${THRESHOLD:-0.1}"
 readonly LAST_ACTIVE_KEY="cpu-utilization/last-active"
 
+function emit() {
+ echo "$(date '+%Y-%m-%d %H:%M:%S') $*"
+}
+readonly -f emit
+
+
 function set_guest_attributes() {
-  echo "Setting cpu-utilization/last-active"
+  emit "Setting cpu-utilization/last-active"
   curl -s -X PUT --data "$1" \
     -H "Metadata-Flavor: Google" \
     "http://metadata.google.internal/computeMetadata/v1/instance/guest-attributes/${LAST_ACTIVE_KEY}"
@@ -27,32 +38,37 @@ function set_guest_attributes() {
 readonly -f set_guest_attributes
 
 function create_tag() {
-  echo "Creating tag vwbapp:cpu-utilization/last-active"
-  INSTANCE_ID="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
+  emit "Creating tag vwbapp:cpu-utilization/last-active"
+  id="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
+  local id
   aws ec2 create-tags \
-    --resources "${INSTANCE_ID}" \
+    --resources "${id}" \
     --tags Key=vwbapp:${LAST_ACTIVE_KEY},Value="$1"
 }
 readonly -f create_tag
 
 function set_cpu_last_active() {
+  local now
   now=$(date +'%s')
   if [[ "${CLOUD}" == "gcp" ]]; then
     set_guest_attributes "${now}"
-  else
+  elif [[ "${CLOUD}" == "aws" ]]; then
     create_tag "${now}"
+  else
+    1>&2 emit "Unexpected cloud platform ${CLOUD}"  
+    exit 1
   fi  
 }
 readonly -f set_cpu_last_active
 
-while true
-do
-
-  load=$(uptime | sed -e 's/.*load average: //g' | awk '{ print $1 }') # 1-minute average load
-  load="${load//,}" # remove trailing comma
-  echo "cpu load is $load"
-  if (( $(echo "$threshold > $load" |bc -l) )); then
-    echo "Idling.."
+declare LOAD
+while true; do
+  LOAD="$(awk '{print $1}' /proc/loadavg)" # 1-minute average load
+  emit "cpu load is $load"
+  # Check if the LOAD has exceeded the THRESHOLD.  
+  # Note the use of awk for comparison of real numbers.  
+  if echo "${THRESHOLD}" "${LOAD}" | awk '{if ($1 > $2) exit 0; else exit 1}'; then
+    emit "Idling.."
   else
     set_cpu_last_active
   fi

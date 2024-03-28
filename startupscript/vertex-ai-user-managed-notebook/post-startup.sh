459a99f49b2b28b51dd16c52c7fadb032893f48d
Yu Hu <yuhuyoyo@google.com>
2024-03-28 17:50:14 -0400

    double quote

--- startupscript/vertex-ai-user-managed-notebook/post-startup.sh
+++ startupscript/vertex-ai-user-managed-notebook/post-startup.sh
@@ -831,8 +831,8 @@ fi
 # Restart proxy to pick up the new env
 ######################################
 readonly IS_NON_GOOGLE_ACCOUNT
-IS_NON_GOOGLE_ACCOUNT="$(curl -X 'GET' "https://${TERRA_SERVER/verily/terra}-user.api.verily.com/api/profile?path=non_google_account" \
-                    -H 'accept: application/json' -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+IS_NON_GOOGLE_ACCOUNT="$(curl "https://${TERRA_SERVER/verily/terra}-user.api.verily.com/api/profile?path=non_google_account" \
+                    -H "accept: application/json" -H "Authorization: Bearer $(gcloud auth print-access-token)" \
                   | jq '.value')"
 
 if [[ "${IS_NON_GOOGLE_ACCOUNT}" == "true" ]]; then

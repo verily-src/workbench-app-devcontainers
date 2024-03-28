d8dc83794de23382bc678656455c1c66293827c0
Yu Hu <yuhuyoyo@google.com>
2024-03-28 11:44:23 -0400

    add readme

--- startupscript/butane/install-devcontainer-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install-devcontainer-cli.sh installs the devcontainer CLI on the VM.
-set -e
-
-echo "install devcontainer cli"
-wget -q -O- "https://storage.googleapis.com/devcontainer_cli/cli-0.54.0.tgz" | tar -xz -C /home/core

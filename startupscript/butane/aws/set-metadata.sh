a3d7c0bae978986a9af34c2f783d7b0aa2f8f4d3
Yu Hu <yuhuyoyo@google.com>
2024-03-28 15:14:33 -0400

    clean up

--- startupscript/butane/aws/set-metadata.sh
+++ startupscript/butane/aws/set-metadata.sh
@@ -6,14 +6,11 @@
 # Sets tags on the EC2 instance with the given key and value.
 function set_metadata() {
   echo "Creating tag vwbapp:$1"
-  readonly AWS_CLI_EXE="docker run --rm public.ecr.aws/aws-cli/aws-cli"
   local id
   id="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
   docker run --rm public.ecr.aws/aws-cli/aws-cli \
     ec2 create-tags \
       --resources "${id}" \
       --tags Key=vwbapp:"$1",Value="$2"
-    --resources "${id}" \
-    --tags Key=vwbapp:"$1",Value="$2"
 }
 readonly -f set_metadata

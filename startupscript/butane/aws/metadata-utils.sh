f3d38bc98a7c00f0a9faaeb1dac6eeee1abeb92f
Yu Hu <yuhuyoyo@google.com>
2024-04-01 16:43:34 -0400

    add a utils class for metadata

---
+++ startupscript/butane/aws/metadata-utils.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# metadata-utils.sh defines helper functions for aws ec2 tags. This script is intended to be sourced from other scripts
+# to retrieve or modify instance tags. It is run on the VM host. AWS CLI is not installed on Flatcar VM by default so
+# we are running the AWS CLI in a container.
+
+# Defines a function to retrieve an instance tag set on the VM.
+function get_metadata_value() {
+  if [[ -z "$1" ]]; then
+    echo "usage: get_metadata_value <tag>"
+    exit 1
+  fi
+  local tag_key=vwbapp:"$1"
+
+  INSTANCE_ID="$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)"
+  docker run --rm public.ecr.aws/aws-cli/aws-cli \
+   ec2 describe-tags \
+    --filters "Name=resource-id,Values=${INSTANCE_ID}" "Name=key,Values=$tag_key" \
+    --query "Tags[0].Value" --output text 2>/dev/null
+}
+readonly -f get_metadata_value
---
+++ startupscript/butane/gcp/metadata-utils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# metadata-utils.sh defines functions for gcp instance attributes and guest attributes. This script is intended to be sourced from other scripts
+# to retrieve or modify instance attributes. It is run on the VM host.
+
+# Defines a function to retrieve an instance attributes set on the VM.
+function get_metadata_value() {
+  if [[ -z "$1" ]]; then
+    echo "usage: get_metadata_value <tag>"
+    exit 1
+  fi
+  local metadata_path="${1}"
+  curl --retry 5 -s -f \
+    -H "Metadata-Flavor: Google" \
+    "http://metadata/computeMetadata/v1/instance/attributes/${metadata_path}"
+}
+readonly -f get_metadata_value 

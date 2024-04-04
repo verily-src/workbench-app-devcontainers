fb3986f77ddb5ba8121ad16f863c6f23fd25ee53
Yu Hu <yuhuyoyo@google.com>
2024-04-04 11:35:53 -0400

    readonly

--- startupscript/butane/idle-shutdown.sh
+++ startupscript/butane/idle-shutdown.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# idle-shutdown.sh shuts down the VM if it has been idle for a certain amount of time.
+# idle-shutdown.sh shuts down the VM if it has been idle for a certain amount of time. This script is
+# run on the host VM by a systemd timer unit to check for inactivity and shut down the VM. By default,
+# the VM will shut down if it has been idle for 48 hours (172800 seconds).
 
 set -o errexit
 set -o nounset
@@ -11,21 +13,22 @@ set -o xtrace
 source /home/core/metadata-utils.sh
 
 # Get the idle timeout in seconds.
-IDLE_TIMEOUT_SECONDS=$(get_metadata_value "idle-timeout-seconds" || echo "172800")
+IDLE_TIMEOUT_SECONDS="$(get_metadata_value "idle-timeout-seconds" || echo "172800")"
 readonly IDLE_TIMEOUT_SECONDS
 
 # Get the last time the VM was active.
-declare LAST_ACTIVE
-LAST_ACTIVE=$(get_guest_attribute "last-active/cpu" || echo "0")
-LAST_ACTIVE_PROXY=$(get_guest_attribute "last-active/proxy" || echo "0")
+LAST_ACTIVE="$(get_guest_attribute "last-active/cpu" || echo "0")"
+LAST_ACTIVE_PROXY="$(get_guest_attribute "last-active/proxy" || echo "0")"
 
 # get the latest time between the two
 if [[ "${LAST_ACTIVE}" -lt "${LAST_ACTIVE_PROXY}" ]]; then
     LAST_ACTIVE="${LAST_ACTIVE_PROXY}"
 fi
+readonly LAST_ACTIVE
 echo "Last active time: ${LAST_ACTIVE}"
 
-NOW=$(date +'%s')
+NOW="$(date +'%s')"
+readonly NOW
 
 # Check if the VM has been idle for longer than the timeout.
 if [[ $((NOW - LAST_ACTIVE)) -gt IDLE_TIMEOUT_SECONDS ]]; then

267dd03d646d3b2dee1ee67b1e4fa3e5b6b70f89
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-04-08 16:17:40 -0400

    fix aws metadata util script (#90)
    * fix aws metadata util script
    
    * Update metadata-utils.sh
    
    * Update startupscript/butane/aws/metadata-utils.sh
    
    Co-authored-by: Matt Bookman <mbookman@google.com>
    
    ---------
    
    Co-authored-by: Matt Bookman <mbookman@google.com>

--- startupscript/butane/aws/metadata-utils.sh
+++ startupscript/butane/aws/metadata-utils.sh
@@ -4,7 +4,7 @@
 # to retrieve or modify instance tags. It is run on the VM host. AWS CLI is not installed on Flatcar VM by default so
 # we are running the AWS CLI in a container.
 
-# Retrieves an instance tag set on the VM.
+# Retrieves an instance tag set on the VM. If the tag is not set, it returns an empty string..
 function get_metadata_value() {
   if [[ -z "$1" ]]; then
     echo "usage: get_metadata_value <tag>"
@@ -12,16 +12,21 @@ function get_metadata_value() {
   fi
   local tag_key=vwbapp:"$1"
 
-  echo "getting EC2 tag for vwbapp:${tag_key}"
   local token
   token=$(wget --method=PUT --header "X-aws-ec2-metadata-token-ttl-seconds:600" -q -O - http://169.254.169.254/latest/api/token)
   local id
   id=$(wget --header "X-aws-ec2-metadata-token: ${token}" -q -O - http://169.254.169.254/latest/meta-data/instance-id)
-  docker run --rm --detach --network host \
+  local tag_value
+  tag_value="$(docker run --rm --network host \
     public.ecr.aws/aws-cli/aws-cli \
     ec2 describe-tags \
     --filters "Name=resource-id,Values=${id}" "Name=key,Values=${tag_key}" \
-    --query "Tags[0].Value" --output text 2>/dev/null
+    --query "Tags[0].Value" --output text 2>/dev/null)"
+  if [[ "${tag_value}" == "None" ]]; then
+    echo ""
+  else
+    echo "${tag_value}"
+  fi
 }
 readonly -f get_metadata_value 
 

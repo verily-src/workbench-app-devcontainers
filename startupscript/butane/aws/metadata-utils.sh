ff71dbf07ae8b84ac4f2fc352975bf83a9929518
Yu Hu <yuhuyoyo@google.com>
2024-05-03 10:31:03 -0400

    use vwbusr for user set tag

--- startupscript/butane/aws/metadata-utils.sh
+++ startupscript/butane/aws/metadata-utils.sh
@@ -5,12 +5,12 @@
 # we are running the AWS CLI in a container.
 
 # Retrieves an instance tag set on the VM. If the tag is not set, it returns the default vaule.
-function get_metadata_value() {
-  if [[ $# -lt 2 ]]; then
-    echo "usage: get_metadata_value <tag> <default-value>"
+function get_tag() {
+  if [[ $# -lt 3 ]]; then
+    echo "usage: get_metadata_value <prefix> <tag> <default-value>"
     exit 1
   fi
-  local tag_key=vwbapp:"$1"
+  local tag_key="$1":"$2"
 
   local token
   token=$(wget --method=PUT --header "X-aws-ec2-metadata-token-ttl-seconds:600" -q -O - http://169.254.169.254/latest/api/token)
@@ -23,16 +23,23 @@ function get_metadata_value() {
     --filters "Name=resource-id,Values=${id}" "Name=key,Values=${tag_key}" \
     --query "Tags[0].Value" --output text 2>/dev/null)"
   if [[ "${tag_value}" == "None" ]]; then
-    echo "${2}"
+    echo "${3}"
   else
     echo "${tag_value}"
   fi
 }
-readonly -f get_metadata_value 
+readonly -f get_tag 
+
+# EC2 instance uses tags instead of metadata. But to keep the iterface consistent with GCP, this method retrieves tags set by the user.
+function get_metadata_value() {
+  get_tag "vwbusr" "${1}" "${2}"
+}
+readonly -f get_metadata_value
 
 # guest attributes are not supported on EC2 instances. But to keep the interface consistent with GCP, we define a no-op function.
+# This method retrieves the attributes that are set from the instance, e.g. scripts running inside the instance.
 function get_guest_attribute() {
-  get_metadata_value "${1}" "${2}"
+  get_tag "vwbapp" "${1}" "${2}"
 }
 readonly -f get_guest_attribute
 

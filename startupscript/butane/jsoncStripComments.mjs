ed1f663d49dc099a350002538d8359236d095eb7
Christopher Book <cbook@google.com>
2024-06-26 13:22:00 -0400

    Add support for 'open with' (#106)
    - define devcontainer customizations to indicate the supported open url.
    - include a package.json to pull in required node packages.
    - also use the package.json for the devcontainer cli install.

---
+++ startupscript/butane/jsoncStripComments.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { stripComments } from "jsonc-parser";
+import fs from "fs"; 
+const jsonc = fs.readFileSync(process.stdin.fd, 'utf-8');
+const json = stripComments(jsonc); 
+fs.writeFileSync(process.stdout.fd, json); 
+fs.writeFileSync(process.stdout.fd, json); 

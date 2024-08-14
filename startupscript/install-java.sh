37e4d50f5001ede31618008921fb339244c71078
Christopher Book <cbook@google.com>
2024-08-14 11:40:46 +0000

    Install Java on libreoffice via a custom feature (#116)

--- startupscript/install-java.sh
+++ startupscript/install-java.sh
@@ -13,6 +13,8 @@
 # - USER_PRIMARY_GROUP: name of primary group app user belongs to
 
 # Check if the user name is provided.
-ln -sf "$(which java)" "/usr/bin"
-chown --no-dereference "${USER_NAME}:${USER_PRIMARY_GROUP}" "/usr/bin/java"
+if [ ! -f "/usr/bin/java" ]; then
+  ln -sf "$(which java)" "/usr/bin"
+  chown --no-dereference "${USER_NAME}:${USER_PRIMARY_GROUP}" "/usr/bin/java"
+fi
 

361da02b01ca7a57eb3c871e55bbbc70acc76957
rogerwangcs <rogerwangcs@gmail.com>
2024-10-10 10:49:38 -0400

    ignore non-zero exit code from resource mount on app creation

--- src/cirrocumulus/scripts/cirro_scanner.sh
+++ src/cirrocumulus/scripts/cirro_scanner.sh
@@ -39,7 +39,7 @@ function scan_folders_and_create_datasets() {
 }
 readonly -f scan_folders_and_create_datasets
 
-wb resource mount
+wb resource mount || true
 
 # Infinite loop to continuously scan every 5 seconds
 while true; do

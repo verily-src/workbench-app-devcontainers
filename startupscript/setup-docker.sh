487ecd6cc93b9438d33b805bf1217beb0edfe0f2
rogerwangcs <rogerwangcs@gmail.com>
2025-01-13 13:17:12 -0500

    tweaks

--- startupscript/setup-docker.sh
+++ startupscript/setup-docker.sh
@@ -12,20 +12,20 @@ set -o xtrace
 # Install Docker CLI #
 ######################
 
-sudo mkdir -p /etc/apt/keyrings && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+mkdir -p /etc/apt/keyrings && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null && \
 
-sudo apt-get update && \
-sudo apt-get install -y docker-ce-cli && \
+apt-get update && \
+apt-get install -y docker-ce-cli && \
 
 ###########################################
 # Add container user to host docker group #
 ###########################################
 
 # Add jupyter user to the host's docker group
-sudo sed "/^docker:/ s/$/,jupyter/" /etc/host-group > /tmp/host-group.modified
-sudo tee /etc/host-group < /tmp/host-group.modified > /dev/null
+sed "/^docker:/ s/$/,jupyter/" /etc/host-group > /tmp/host-group.modified
+tee /etc/host-group < /tmp/host-group.modified > /dev/null
 
 # create a matching docker group in the container and add the user to it
 DOCKER_GID=$(grep '^docker:' "/etc/host-group" | cut -d: -f3)
@@ -39,7 +39,7 @@ usermod -aG docker jupyter
 ###########################
 
 # Give user write permissions to the mounted docker config directory
-sudo chown -R jupyter /home/jupyter/.docker
+chown -R jupyter /home/jupyter/.docker
 
-# Configure docker auth to use gcloud CLI for gcr.io
-sudo -u jupyter bash -c 'gcloud auth configure-docker --quiet'
+# Login to docker with gcloud credentials (needs to be re-run every 30 min if needed)
+docker login -u oauth2accesstoken -p $(gcloud auth print-access-token) https://us-central1-docker.pkg.dev

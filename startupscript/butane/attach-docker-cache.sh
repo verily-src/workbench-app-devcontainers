98458cffdfbf395f21eb4be708ceaf6a1c564d19
David Shen <shendavid@verily.com>
2025-03-17 10:59:14 -0400

    More linting fixes

--- startupscript/butane/attach-docker-cache.sh
+++ startupscript/butane/attach-docker-cache.sh
@@ -21,30 +21,40 @@ function attach {
   mkdir -p "/dc/${id}"
   mount -o ro "${1}" "/dc/${id}"
 
-  # Copy the overlay2 directories to the docker cache, symlinking the diff
-  # directories and excluding the work and merged directories. Skip directories
-  # that already exist. We are skipping the "l" directory for now, since it
-  # contains relative symlinks that we will copy directly in the next step
-  find /dc/${id}/overlay2 -maxdepth 1 -mindepth 1 -type d ! -name "l" \
+  # Copy the overlay2 directories to the docker cache
+  find_args=(
+      "/dc/${id}/overlay2" -maxdepth 1 -mindepth 1 -type d
+
+      # Skip the "l" directory for now, since it contains relative symlinks that
+      # we will copy directly in the next step
+      ! -name "l"
+
       # If the directory already exists, skip the remaining steps
-      -execdir bash -c '[ ! -d "/var/lib/docker/overlay2/${1}" ] || (echo "Skipping existing directory ${1}"; exit 1)' shell "{}" \; \
+      # shellcheck disable=SC2016
+      -execdir bash -c '[ ! -d "/var/lib/docker/overlay2/${1}" ] || (echo "Skipping existing directory ${1}"; exit 1)' shell "{}" \;
+
       # Copy the layer's metadata, preserving all file permissions. diff will be
       # handled in the next step, while work and merged are temporary
       # directories that we do not want to include
-      -execdir bash -c 'rsync -a --exclude="diff" --exclude "work" --exclude "merged" "${1}" /var/lib/docker/overlay2/' shell "{}" \; \
+      # shellcheck disable=SC2016
+      -execdir bash -c 'rsync -a --exclude="diff" --exclude "work" --exclude "merged" "${1}" /var/lib/docker/overlay2/' shell "{}" \;
+
       # The diff directory contains the actual data. Since overlayfs is able to
       # understand symlinks, we can just symlink the diff directory to the read-only
       # cache. We cannot simply symlink the entire directory, since docker needs
       # to create read-write work and merged directories to merge the layers
+      # shellcheck disable=SC2016
       -execdir bash -c 'ln -s "/dc/${2}/overlay2/${1}/diff" "/var/lib/docker/overlay2/${1}/diff"' shell "{}" "${id}" \;
+  )
+  find "${find_args[@]}"
 
   # Copy the links, ignoring existing files. These are relative symlinks, of the
   # format <short_id> -> ../<long_layer_id>
-  rsync --ignore-existing -a /dc/${id}/overlay2/l/ /var/lib/docker/overlay2/l
+  rsync --ignore-existing -a "/dc/${id}/overlay2/l/" "/var/lib/docker/overlay2/l"
 
   # Copy the required image directories, ignoring existing files
-  rsync --ignore-existing -a /dc/${id}/image/overlay2/distribution/diffid-by-digest/sha256/ /var/lib/docker/image/overlay2/distribution/diffid-by-digest/sha256
-  rsync --ignore-existing -a /dc/${id}/image/overlay2/layerdb/sha256/ /var/lib/docker/image/overlay2/layerdb/sha256
+  rsync --ignore-existing -a "/dc/${id}/image/overlay2/distribution/diffid-by-digest/sha256/" "/var/lib/docker/image/overlay2/distribution/diffid-by-digest/sha256"
+  rsync --ignore-existing -a "/dc/${id}/image/overlay2/layerdb/sha256/" "/var/lib/docker/image/overlay2/layerdb/sha256"
 }
 readonly -f attach
 export -f attach
@@ -63,5 +73,7 @@ mkdir -p /var/lib/docker/image/overlay2/layerdb/sha256
 chmod 700 -R /var/lib/docker/image
 
 # Look for disk cache partitions, with priority to vwb caches
+# shellcheck disable=SC2016
 find /dev/disk/by-id/ -maxdepth 1 -name "scsi-0Google_PersistentDisk_vwb-docker-cache-*-part*" -exec bash -c 'attach "${1}"' shell "{}" \;
+# shellcheck disable=SC2016
 find /dev/disk/by-id/ -maxdepth 1 -name "scsi-0Google_PersistentDisk_docker-cache-*-part*" -exec bash -c 'attach "${1}"' shell "{}" \;

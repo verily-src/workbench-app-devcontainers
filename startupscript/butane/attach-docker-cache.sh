e14416341b92ea9ac0cc67b2e6b3af81164e5a45
David Shen <shendavid@verily.com>
2025-03-17 12:08:03 -0400

    Add comments explaining cache priority; variable names

--- startupscript/butane/attach-docker-cache.sh
+++ startupscript/butane/attach-docker-cache.sh
@@ -10,50 +10,60 @@ set -o pipefail
 set -o xtrace
 
 function attach {
-  if findmnt "${1}" > /dev/null; then
-    echo "${1} is already mounted"
+  PS4="++ "
+  set -o errexit
+  set -o nounset
+  set -o pipefail
+  set -o xtrace
+
+  local DISK_NAME="${1}"
+  local DISK_UUID="$(blkid -o value -s UUID "${DISK_NAME}")"
+  local MOUNTPOINT="/dc/${DISK_UUID}"
+  local DOCKER_ROOT="/var/lib/docker"
+
+  if findmnt "${DISK_NAME}" > /dev/null; then
+    echo "${DISK_NAME} is already mounted"
     return
   fi
 
   # Mount the read-only cache to a directory by its UUID, so that it will be
   # mounted at the same point across reboots
-  id="$(blkid -o value -s UUID "${1}")"
-  mkdir -p "/dc/${id}"
-  mount -o ro "${1}" "/dc/${id}"
+  mkdir -p "${MOUNTPOINT}"
+  mount -o ro "${DISK_NAME}" "${MOUNTPOINT}"
 
   # Copy the overlay2 directories to the docker cache
   # shellcheck disable=SC2016
   find_args=(
-      "/dc/${id}/overlay2" -maxdepth 1 -mindepth 1 -type d
+    "${MOUNTPOINT}/overlay2" -maxdepth 1 -mindepth 1 -type d
 
-      # Skip the "l" directory for now, since it contains relative symlinks that
-      # we will copy directly in the next step
-      ! -name "l"
+    # Skip the "l" directory for now, since it contains relative symlinks that
+    # we will copy directly in the next step
+    ! -name "l"
 
-      # If the directory already exists, skip the remaining steps. Pass the file
-      # name as a shell parameter to avoid malicious injection.
-      -execdir bash -c '[ ! -d "/var/lib/docker/overlay2/${1}" ] || (echo "Skipping existing directory ${1}"; exit 1)' shell "{}" \;
+    # If the directory already exists, skip the remaining steps. Pass the file
+    # name as a shell parameter to avoid malicious injection.
+    -execdir bash -c '[ ! -d "${1}" ] || (echo "Skipping existing directory ${1}"; exit 1)' shell "${DOCKER_ROOT}/overlay2/{}" \;
 
-      # Copy the layer's metadata, preserving all file permissions. diff will be
-      # handled in the next step, while work and merged are temporary
-      # directories that we do not want to include
-      -execdir rsync -a --exclude="diff" --exclude "work" --exclude "merged" "{}" /var/lib/docker/overlay2/ \;
+    # Copy the layer's metadata, preserving all file permissions. diff will be
+    # handled in the next step, while work and merged are temporary
+    # directories that we do not want to include
+    -execdir rsync -a --exclude="diff" --exclude "work" --exclude "merged" "{}" /var/lib/docker/overlay2/ \;
 
-      # The diff directory contains the actual data. Since overlayfs is able to
-      # understand symlinks, we can just symlink the diff directory to the read-only
-      # cache. We cannot simply symlink the entire directory, since docker needs
-      # to create read-write work and merged directories to merge the layers
-      -execdir ln -s "/dc/${id}/overlay2/{}/diff" "/var/lib/docker/overlay2/{}/diff" \;
+    # The diff directory contains the actual data. Since overlayfs is able to
+    # understand symlinks, we can just symlink the diff directory to the read-only
+    # cache. We cannot simply symlink the entire directory, since docker needs
+    # to create read-write work and merged directories to merge the layers
+    -execdir ln -s "${MOUNTPOINT}/overlay2/{}/diff" "${DOCKER_ROOT}/overlay2/{}/diff" \;
   )
   find "${find_args[@]}"
 
   # Copy the links, ignoring existing files. These are relative symlinks, of the
   # format <short_id> -> ../<long_layer_id>
-  rsync --ignore-existing -a "/dc/${id}/overlay2/l/" "/var/lib/docker/overlay2/l"
+  rsync --ignore-existing -a "${MOUNTPOINT}/overlay2/l/" "${DOCKER_ROOT}/overlay2/l"
 
   # Copy the required image directories, ignoring existing files
-  rsync --ignore-existing -a "/dc/${id}/image/overlay2/distribution/diffid-by-digest/sha256/" "/var/lib/docker/image/overlay2/distribution/diffid-by-digest/sha256"
-  rsync --ignore-existing -a "/dc/${id}/image/overlay2/layerdb/sha256/" "/var/lib/docker/image/overlay2/layerdb/sha256"
+  rsync --ignore-existing -a "${MOUNTPOINT}/image/overlay2/distribution/diffid-by-digest/sha256/" "${DOCKER_ROOT}/image/overlay2/distribution/diffid-by-digest/sha256"
+  rsync --ignore-existing -a "${MOUNTPOINT}/image/overlay2/layerdb/sha256/" "${DOCKER_ROOT}/image/overlay2/layerdb/sha256"
 }
 readonly -f attach
 export -f attach
@@ -71,7 +81,12 @@ mkdir -p /var/lib/docker/image/overlay2/distribution/diffid-by-digest/sha256
 mkdir -p /var/lib/docker/image/overlay2/layerdb/sha256
 chmod 700 -R /var/lib/docker/image
 
-# Look for disk cache partitions, with priority to vwb caches
+# Look for disk cache partitions, with priority to vwb caches. The attach
+# function ignores anything that already exists, so by attaching the first-party
+# VWB caches first, we can establish the priority:
+#   1. local files
+#   2. VWB caches
+#   3. user-supplied caches
 # shellcheck disable=SC2016
 find /dev/disk/by-id/ -maxdepth 1 -name "scsi-0Google_PersistentDisk_vwb-docker-cache-*-part*" -exec bash -c 'attach "${1}"' shell "{}" \;
 # shellcheck disable=SC2016

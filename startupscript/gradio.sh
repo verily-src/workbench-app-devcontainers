15326cec49730803883ff881094e1fe3765d511a
Tucker Reinhardt <reinhardtt@verily.health>
2025-05-07 22:59:10 +0000

    change some things

--- startupscript/gradio.sh
+++ startupscript/gradio.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
-if [ ! -f /workspace/verily1/main.py ]; then
-  echo "Cloning repo into /workspace/verily1..."
-  git clone git@github.com:verily-src/verily1.git /workspace/verily1
-else
-  echo "Repo already exists at /workspace/verily1"
-fi
+echo "Cloning repo into /workspace/verily1..."
+git clone git@github.com:verily-src/verily1.git verily1
 
-cd workspace/verily1
+cd verily1
 git checkout fhir-agent
 
 # Install Python dependencies

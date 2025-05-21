e2c8b20e2a53cd986f49a0d9dd06cc2edd6d5561
David Shen <2984600+pantherman594@users.noreply.github.com>
2025-05-21 13:47:38 -0400

    Add shutdown option with devcontainer failure handler (#183)

--- startupscript/butane/devcontainer-failure-handler.sh
+++ startupscript/butane/devcontainer-failure-handler.sh
@@ -8,6 +8,18 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+function usage {
+  echo "Usage: $0 <shutdown_on_failure>"
+  echo "  shutdown_on_failure: whether to shut down the system after running out of retries."
+  exit 1
+}
+
+# Check that the required arguments are provided: shutdown_on_failure
+if [[ $# -lt 1 ]]; then
+    usage
+fi
+
+readonly SHUTDOWN_ON_FAILURE="$1"
 readonly RETRY_COUNT=3
 readonly RETRY_FILE="/tmp/devcontainer-failure-count"
 
@@ -24,4 +36,10 @@ if [[ "${num_retries}" -ge "${RETRY_COUNT}" ]]; then
 
         # Stop the service
         systemctl stop devcontainer.service
+
+        if [[ "$SHUTDOWN_ON_FAILURE" == "true" ]]; then
+            # Sleep for a bit in case logs need to be captured
+            sleep 10
+            systemctl poweroff
+        fi
 fi

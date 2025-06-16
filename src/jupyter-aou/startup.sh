e694e7caab740c4c529b64d8b0e860a9f6d637d1
David Shen <2984600+pantherman594@users.noreply.github.com>
2025-06-16 11:24:29 -0400

    Move fuse commands to a sidecar for jupyter-aou (#178)
    * fuse in a sidecar
    
    * Pass arguments as a JSON string since eval is susceptible to injection
    
    * Fix remotefuse cleanup
    
    * comment
    
    * Update image version

---
+++ src/jupyter-aou/startup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script is a custom startup script for jupyter aou. It is used to generate
+# an SSH key pair for the jupyter user to be used in the remotefuse sidecar.
+# /ssh-keys should be a volume mounted to both containers. It also sets
+# permissions on /home/jupyter/workspace to allow the jupyter user to read/write to
+# it.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly USER_NAME="jupyter"
+readonly RUN_AS_LOGIN_USER="sudo -u ${USER_NAME} bash -l -c"
+
+rm -rf "/home/${USER_NAME}/.ssh"
+${RUN_AS_LOGIN_USER} "mkdir -p '/home/${USER_NAME}/.ssh'"
+${RUN_AS_LOGIN_USER} "ssh-keygen -q -f '/home/${USER_NAME}/.ssh/remotefuse' -N ''"
+cp "/home/${USER_NAME}/.ssh/remotefuse.pub" /ssh-keys/remotefuse.pub
+
+# The remaining commands are expected to fail if this is not the first run
+set +o errexit
+
+# This will fail if any resources are already mounted, since the mounted
+# resources can't be chowned
+chown -R ${USER_NAME}:users "/home/${USER_NAME}/workspace"
+
+# Modify the startup script so that /opt/remotefuse always takes priority over
+# /usr/bin
+sed -i 's/export PATH=\/usr\/bin:/export PATH=\/opt\/remotefuse:\/usr\/bin:/g' /workspace/startupscript/post-startup.sh

35814898a2f0b5b3fc7ac971af8c6bc073b1eb9c
David Shen <shendavid@verily.com>
2025-09-09 13:30:21 -0400

    Install workbench-tools using conda

---
+++ features/src/workbench-tools/install-conda.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# install-conda.sh installs conda and mamba into the devcontainer.
+# Installation steps taken from https://github.com/rocker-org/devcontainer-features/blob/main/src/miniforge/install.sh
+
+install_miniforge() {
+    local conda_dir=$1
+    local download_url
+    download_url="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-$(uname -m).sh"
+
+    check_packages curl ca-certificates
+    mkdir -p /tmp/miniforge
+    pushd /tmp/miniforge
+    curl -sLo miniforge.sh "${download_url}"
+    chmod +x miniforge.sh
+    /bin/bash miniforge.sh -b -p "${conda_dir}"
+    popd
+    rm -rf /tmp/miniforge
+    "${conda_dir}/bin/conda" clean -yaf
+}
+
+if ! grep -e "^conda:" "/etc/group" >/dev/null 2>&1; then
+    groupadd -r conda
+fi
+usermod -a -G conda "${USERNAME}"
+
+check_packages curl ca-certificates
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" >/etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
+# Install the miniforge
+echo "Downloading Miniforge3..."
+install_miniforge "${CONDA_DIR}"
+
+CONDA_SCRIPT="${CONDA_DIR}/etc/profile.d/conda.sh"
+# shellcheck source=/dev/null
+source "${CONDA_SCRIPT}"
+conda config --set env_prompt '({name})'
+
+echo "source ${CONDA_SCRIPT}" >>"/${USER_HOME_DIR}/.bashrc"
+chown -R "${USERNAME}:${USERNAME}" "/${USER_HOME_DIR}/.bashrc"
+
+chown -R "${USERNAME}:conda" "${CONDA_DIR}"
+chmod -R g+r+w "${CONDA_DIR}"
+find "${CONDA_DIR}" -type d -print0 | xargs -n 1 -0 chmod g+s
--- test/test.sh
+++ test/test.sh
@@ -7,8 +7,12 @@ readonly TEMPLATE_ID="$1"
 readonly APPS_WITH_WORKBENCH_TOOLS=(
     "custom-workbench-jupyter-template"
     "jupyter-aou"
+    "nemo_jupyter"
+    "nemo_jupyter_aou"
     "r-analysis"
     "vscode"
+    "workbench-jupyter-parabricks"
+    "workbench-jupyter-parabricks-aou"
 )
 
 HAS_WORKBENCH_TOOLS="false"
@@ -22,35 +26,37 @@ readonly HAS_WORKBENCH_TOOLS
 
 
 # Template specific tests
-check "gcsfuse" which gcsfuse
-check "wb cli" which wb
+check "gcsfuse" gcsfuse -v
+check "wb cli" wb version
 check "fuse.conf user_allow_other" grep -qE "^[[:space:]]*[^#]*user_allow_other" "/etc/fuse.conf"
-if [[ "$TEMPLATE_ID" != "nemo_jupyter" ]] && [[ "$TEMPLATE_ID" != "nemo_jupyter_aou" ]]; then
-    check "cromwell" test -e "${CROMWELL_JAR}"
-fi
-check "nextflow" which nextflow
-check "dsub" which dsub
 
 # The workbench-tools feature should install these
 if [[ "$HAS_WORKBENCH_TOOLS" == "true" ]]; then
-    # TODO(PHP-80766): Enable the disabled checks when caching is ready
-    check "python3" which python3
-    # check "bcftools" which bcftools
-    check "pip3" which pip3
-    # check "bgenix" which bgenix
-    check "plink" which plink
-    check "plink2" which plink2
-    # check "samtools" which samtools
-    # check "bgzip" which bgzip
-    # check "tabix" which tabix
-    # check "vcftools: fill-an-ac" which fill-an-ac
-    # check "vcftools: fill-fs" which fill-fs
-    check "regenie" which regenie
-    check "regenie_mkl" which regenie_mkl
-    # check "vep" which vep
-    # check "vep: filter_vep" which filter_vep
-    # check "vep: variant_recoder" which variant_recoder
-    # check "vep: haplo" which haplo
+    check "python3" python3 --version
+    check "pip3" pip3 --version
+    if [[ "$TEMPLATE_ID" != "nemo_jupyter" ]] && [[ "$TEMPLATE_ID" != "nemo_jupyter_aou" ]]; then
+        check "cromwell" cromwell --version
+    fi
+    check "nextflow" nextflow -v
+    check "dsub" dsub -v
+    check "bcftools" bcftools --version
+    check "bedtools" bedtools --version
+    check "bgenix" bgenix -help
+    check "plink" plink --version
+    check "plink2" plink2 --version
+    check "samtools" samtools --version
+    check "bgzip" bgzip --version
+    check "tabix" tabix --version
+    check "vcftools" vcftools --version
+    # fill-an-ac -h returns 1, so grep the usage string instead
+    check "vcftools: fill-an-ac" bash -c 'fill-an-ac -h 2>&1 | grep "Usage: fill-an-ac"'
+    # fill-fs -h returns 1, so grep the usage string instead
+    check "vcftools: fill-fs" bash -c 'fill-fs -h 2>&1 | grep "Usage: fill-fs"'
+    check "regenie" regenie --version
+    check "vep" vep --help
+    check "vep: filter_vep" filter_vep --help
+    check "vep: variant_recoder" variant_recoder --help
+    check "vep: haplo" haplo --help
 fi
 
 # Report result

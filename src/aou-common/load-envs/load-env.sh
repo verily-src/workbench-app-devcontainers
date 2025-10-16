408fdb8d4ab1211d8db2a880dccd6dd0b86a387d
David Shen <shendavid@verily.com>
2025-10-16 15:43:07 -0400

    Fix script

--- src/aou-common/load-envs/load-env.sh
+++ src/aou-common/load-envs/load-env.sh
@@ -13,27 +13,40 @@ function get_env_vars() {
     set -o nounset
     set -o pipefail
 
+    if [ -f "${HOME}/.aou-env" ]; then
+        cat "${HOME}/.aou-env"
+        return 0
+    fi
+
     CONTEXT_PATH="${HOME}/.workbench/context.json"
     readonly CONTEXT_PATH
 
-    WORKSPACE_ID="$(jq -r '.workspace.userFacingId' "${CONTEXT_PATH}" || echo "")"
-    while [ -z "${WORKSPACE_ID}" ]; do
-      echo "Waiting for Workbench context to be set..."
-      sleep 5
-      WORKSPACE_ID="$(jq -r '.workspace.userFacingId' "${CONTEXT_PATH}" || echo "")"
-    done
+    # Check if the context file exists.
+    if [ ! -f "${CONTEXT_PATH}" ]; then
+        echo "Unable to determine your current workspace. Has the workbench CLI been initialized?" >&2
+        return 1
+    fi
+
+    WORKSPACE_ID="$(jq -r '.workspace.userFacingId' "${CONTEXT_PATH}")"
     readonly WORKSPACE_ID
 
     WSM_API_URL="$(jq -r '.server.workspaceManagerUri' "${CONTEXT_PATH}")"
     readonly WSM_API_URL
 
+    if wb auth status 2>&1 | grep -q "NO USER LOGGED IN"; then
+        echo "You are not logged in. Please run 'wb auth login' to log in." >&2
+        return 1
+    fi
+
     local auth_token
     auth_token="$(wb auth print-access-token)"
 
-    AUTH_TOKEN="$auth_token" ./load-env -workspace "${WORKSPACE_ID}" -wsm-url "${WSM_API_URL}"
+    ENV_VARS="$(AUTH_TOKEN="$auth_token" ./load-env -workspace "${WORKSPACE_ID}" -wsm-url "${WSM_API_URL}")"
+    echo "${ENV_VARS}" > "${HOME}/.aou-env"
+    echo "${ENV_VARS}"
 }
 
-ENV_VARS="$(get_env_file)"
+ENV_VARS="$(get_env_vars)"
 ENV_VARS_RETVAL="$?"
 readonly ENV_VARS
 readonly ENV_VARS_RETVAL
@@ -41,7 +54,7 @@ readonly ENV_VARS_RETVAL
 # If get_env_vars fails, print the error message and exit.
 if [ "${ENV_VARS_RETVAL}" -ne 0 ]; then
     echo "${ENV_VARS}"
-    exit "${ENV_VARS_RETVAL}"
+    return 1
 fi
 
 eval "${ENV_VARS}"

d62d106439abafac38a539f953a6ca27765c8c90
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2026-01-02 10:57:02 -0500

    Cleanup OS update metadata in cleanup script
    Remove unnecessary OS update metadata settings.

--- startupscript/butane/metadata-cleanup.sh
+++ startupscript/butane/metadata-cleanup.sh
@@ -16,5 +16,3 @@ source /home/core/metadata-utils.sh
 set_metadata "notebooks/last_activity" ""
 set_metadata "startup_script/status" ""
 set_metadata "startup_script/message" ""
-set_metadata "os_update/reboot_required" ""
-set_metadata "os_update/timestamp" ""

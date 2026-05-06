7d668e9b4af1388f3996c736fd3fe1f2748a5bb5
David Shen <shendavid@verily.com>
2026-05-06 14:33:59 -0400

    Fix test

--- tests/test-app-secrets.sh
+++ tests/test-app-secrets.sh
@@ -17,7 +17,7 @@ fi
 echo "Injecting mock secrets..."
 echo '[
     {"type":"valueVar","value":"secret-value","target":"SECRET_VALUE"},
-    {"type":"pathVar","value":"secret-value","target":"SECRET_PATH"}
+    {"type":"pathVar","value":"secret-value","target":"SECRET_PATH"},
     {"type":"pipeVar","value":"secret-value","target":"SECRET_PIPE"}
 ]' | timeout 30 docker exec --user root -i "$CONTAINER_NAME" sh -c 'cat > /tmp/secrets'
 

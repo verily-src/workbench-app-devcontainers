cee732f588144e7f07c51f0a5fa65bc82cb19450
Alexander Sullivan <alexander.joohyun.sullivan@gmail.com>
2026-09-21 15:52:41 -0400

    Update Node runtimes to v24 (#479)
    * update to node v24.19.0
    
    * update feature-versions/state.json
    
    * update to node v24.20 because 2 weeks have passed

--- startupscript/butane/010-install-node.sh
+++ startupscript/butane/010-install-node.sh
@@ -10,15 +10,15 @@ set -o pipefail
 set -o xtrace
 
 # Download Node.js from the source code and extract it to /opt
-readonly NODE_VERSION="v18.16.1"
+readonly NODE_VERSION="v24.20.0"
 readonly PLATFORM="linux-x64"
 readonly NODE_TAR="node-${NODE_VERSION}-${PLATFORM}.tar.gz"
-readonly NODE_INSTALL_SRC="https://storage.googleapis.com/bkt-workbench-artifacts/mirror/${NODE_TAR}"
+readonly NODE_INSTALL_SRC="https://nodejs.org/dist/${NODE_VERSION}/${NODE_TAR}"
 readonly NODE_INSTALL_PATH="/home/core/${NODE_TAR}"
 
 echo "Downloading Node from ${NODE_INSTALL_SRC}" 
 wget -q -O "${NODE_INSTALL_PATH}" "${NODE_INSTALL_SRC}"
-echo "59582f51570d0857de6333620323bdeee5ae36107318f86ce5eca24747cabf5b  ${NODE_INSTALL_PATH}" | sha256sum -c
+echo "855d581f8a4eb1a8117e3426de25fe02770592febcfb31369aee1ffbfee9e8ec  ${NODE_INSTALL_PATH}" | sha256sum -c
 
 echo "Installing Node from ${NODE_INSTALL_PATH}" 
 tar -xzf "${NODE_INSTALL_PATH}" -C /opt --strip-components=1
